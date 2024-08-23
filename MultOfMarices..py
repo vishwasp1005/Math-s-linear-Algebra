510ba4927b984e0d8d30038daab37d1d3c94b845
VISHWAS76 <140800019+vishwasp1005@users.noreply.github.com>
2024-08-23 08:06:02 +0530

    Add files via upload

---
+++ MultOfMarices..py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# A = np.array([[3, 4], [5, 6], [7, 8]])
+# b = np.array([1, 2])
+
+# print(np.dot(A, b))
+
+A = np.array([[3, 4], [5, 6], [7, 8]])
+B = np.array([[1, 9], [2, 0]])
+
+print(np.dot(A, B))
